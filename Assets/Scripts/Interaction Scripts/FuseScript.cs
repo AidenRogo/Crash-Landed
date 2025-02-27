55e27754cb80a9e664cd94fb8225daf4838d74da
QuinnNeff <neffq5306@my.uwstout.edu>
2025-02-27 00:39:27 -0600

    Demo Scene 1
    demo of all added game mechanics, and set up in a demo scene for play/test.

--- Assets/Scripts/Interaction Scripts/FuseScript.cs
+++ Assets/Scripts/Interaction Scripts/FuseScript.cs
@@ -1,38 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using System.Threading;
 using UnityEngine;
 
-public class ElevatorOpenLeft : MonoBehaviour
+public class FuseScript : MonoBehaviour
 {
     public GameObject elevatorDoorLeft; // Reference to the LockedDoor GameObject
+    public GameObject generator;
     public float speed = 3;
+    public int count = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (elevatorDoorLeft == null)
+        if (generator == null)
         {
-            Debug.LogError("ElevatorDoorLeft reference is not set in the Inspector.");
+            Debug.LogError("generator reference is not set in the Inspector.");
         }
     }
 
-    // Update is called once per frame
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Fuse")&& count ==0)
         {
-            Debug.Log("Button pressed");
-            if (elevatorDoorLeft != null)
+            Debug.Log("Fuse Touches Generator");
+            if (generator != null)
             {
-
+                count++;
                 elevatorDoorLeft.transform.Translate(Vector2.left * speed * Time.deltaTime, Space.World);
-                
+               
             }
             else
             {
-                Debug.LogError("ElevatorDoorLeft reference is not set.");
+                Debug.LogError("generator reference is not set.");
             }
         }
     }
+
 }
---
+++ Assets/Scripts/Interaction Scripts/Generator.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    // This script can be empty or contain any additional functionality for the Generator
+}
