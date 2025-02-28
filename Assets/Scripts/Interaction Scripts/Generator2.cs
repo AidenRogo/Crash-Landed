5054a7ddec2a64c3fea1694fe76e763ad410e2bc
QuinnNeff <neffq5306@my.uwstout.edu>
2025-02-28 14:55:19 -0600

    Deleted Wall Prefabs

--- Assets/Scripts/Interaction Scripts/Generator2.cs
+++ Assets/Scripts/Interaction Scripts/Generator2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Generator2 : MonoBehaviour
+public class Generator : MonoBehaviour
 {
     // This script can be empty or contain any additional functionality for the Generator
 }
