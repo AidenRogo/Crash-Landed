afaf551dd3a867327ce7daf6080f70585ea9c3c0
AlexLietha <alexlietha@gmail.com>
2025-03-12 15:36:33 -0500

    Added wrench
    just for now it is just a circle hitbox around the character
    
    nuked 2 movement scripts

--- Assets/Scripts/Interaction Scripts/lockedDoorButton.cs
+++ Assets/Scripts/Interaction Scripts/lockedDoorButton.cs
@@ -19,7 +19,7 @@ public class LockedDoorButton : MonoBehaviour
     // Method to detect collision with the player
     void OnTriggerEnter2D(Collider2D other) // Ensure that the collision is 2D
     {
-        if (other.CompareTag("Player")) // Check if object colliding with the button is tagged with "Player"
+        if (other.CompareTag("Wrench")) // Check if object colliding with the button is tagged with "Player"
         {
             Debug.Log("Button pressed"); // Log in console that the button was pressed
             if (lockedDoor != null) // Runs the function if the door is not null
---
+++ Assets/Scripts/swingSword.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class swingSword : MonoBehaviour
+{
+    private bool isSwinging;
+    public GameObject wrenchHitBox;
+    public GameObject visualHitBox;
+    public Input playerControls;
+
+    private InputAction fire;
+
+    private void Awake()
+    {
+        playerControls = new Input();
+    }
+        
+    private void OnEnable()
+    {
+        fire = playerControls.Player.Fire;
+        fire.Enable();
+        fire.performed += Fire;
+    }
+    private void OnDisable()
+    {
+        fire.Disable();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isSwinging = false;
+        wrenchHitBox.SetActive(false);
+        visualHitBox.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Fire(InputAction.CallbackContext context)
+    {
+        
+        if (!isSwinging)
+        {
+            Debug.Log("Swung Wrench");
+            StartCoroutine(Swing());
+
+        }
+
+    }
+
+    IEnumerator Swing()
+    {
+        isSwinging = true;
+        yield return new WaitForSeconds(.5f);
+        wrenchHitBox.SetActive(true);
+        visualHitBox.SetActive(true);
+        yield return new WaitForSeconds(.5f);
+        wrenchHitBox.SetActive(false);
+        visualHitBox.SetActive(false);
+        isSwinging=false;
+    }
+}
