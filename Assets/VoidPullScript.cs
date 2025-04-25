08bc4ea1e39380e077227d822898ce941a203c20
ZacharySuhajda <suhajdaz3219@my.uwstout.edu>
2025-04-25 16:38:35 -0500

    stuff

---
+++ Assets/VoidPullScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidPullScript : MonoBehaviour
+{
+
+    public SpringJoint2D spring;
+    public GameObject player;
+    public Rigidbody2D playerBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            spring.connectedBody = collision.GetComponentInParent<Rigidbody2D>();
+        }
+    }
+}
