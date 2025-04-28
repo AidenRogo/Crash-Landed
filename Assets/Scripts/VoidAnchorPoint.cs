bf5256b2f4dc3377a65415d3e8f639761eab8d9f
ZacharySuhajda <suhajdaz3219@my.uwstout.edu>
2025-04-28 03:54:21 -0500

    TILESSSSSSS

---
+++ Assets/Scripts/VoidAnchorPoint.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidAnchorPoint : MonoBehaviour
+{
+    public Vector2 anchorPoint = new Vector2();
+}
--- Assets/Scripts/VoidKill.cs
+++ Assets/Scripts/VoidKill.cs
@@ -10,31 +10,47 @@ public class VoidKill : MonoBehaviour
     private Color colorToTurnTo = Color.black;
 
     public float shrinkSpeed = 3.5f;
+    public float minSize = 0.05f;
     private float time = 0;
-    private Vector3 spawnPoint;
 
-    private PlayerController controller;
-    private Renderer spriteRenderer;
+    public PlayerController controller;
+    public Renderer spriteRenderer;
 
     // SPRING PLAYER PULL
 
+    public SpringJoint2D playerSpring;
+    public Collider2D hitBox;
+
 
     // SET SPAWN POINT
+
+    private Vector3 spawnPoint;
+
 
 
     void Start()
     {
         spawnPoint = transform.position;
+        playerSpring.enabled = false;
 
-        controller = GetComponent<PlayerController>();
-        spriteRenderer = GetComponent<Renderer>();
+        //hitBox = GetComponent<Collider2D>();
+        //spriteRenderer = GetComponent<Renderer>();
+        //controller = GetComponent<PlayerController>();
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (other.CompareTag("Void"))
+        if (collision.CompareTag("Void"))
         {
+            playerSpring.connectedBody = collision.GetComponent<Rigidbody2D>();
+            playerSpring.connectedAnchor = collision.GetComponent<VoidAnchorPoint>().anchorPoint;
+            hitBox.enabled = false;
+            playerSpring.enabled = true;
             StartCoroutine(Shrink());
+        }
+        else if (collision.CompareTag("SpawnPointSet"))
+        {
+            spawnPoint = collision.transform.position;
         }
     }
     private IEnumerator Shrink()
@@ -53,12 +69,14 @@ public class VoidKill : MonoBehaviour
         }
 
         time = 0;
+        playerSpring.enabled = false;
         yield return new WaitForSeconds(0.2f);
         transform.localScale = Vector3.one;
         spriteRenderer.material.color = Color.white;
         float dist = Vector3.Distance(spawnPoint, transform.position);
         transform.position = spawnPoint;
         yield return new WaitForSeconds(dist * 0.12f);
+        hitBox.enabled = true;
         controller.move.Enable();
     }
    
