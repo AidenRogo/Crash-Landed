10761071c97136a4c82b7cee0ea204e490592f58
AlexLietha <alexlietha@gmail.com>
2025-04-28 14:55:31 -0500

    highlights and charater animations update

--- Assets/Scripts/Interaction Scripts/Lever.cs
+++ Assets/Scripts/Interaction Scripts/Lever.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 /*public class LockedDoorButton : MonoBehaviour
@@ -42,6 +44,9 @@ public class Lever : MonoBehaviour
 {
     public LockedDoor lockedDoor; // Reference to the LockedDoor component
     public VerticalLockedDoor verticalLockedDoor; // Reference to the VerticalLockedDoor component
+    public PlayerController playerController;
+    public HighlightInteractable highlight;
+    private int idk;
 
     private bool isFlipped = false; // Flag to check if the lever is flipped
 
@@ -55,9 +60,40 @@ public class Lever : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Wrench")) // Check if the player collides with the lever
+        if (other.CompareTag("Wrench"))
+        {
+            highlight.Highlight();
+
+        }
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        
+            if ((other.CompareTag("Wrench") && playerController.isSwinging && !verticalLockedDoor.isMoving)) // Check if the player collides with the lever
+            {
+
+                isFlipped = !isFlipped; // Toggle the lever state
+                StartCoroutine(OpenVerticleDoor());
+            }
+        else if((other.CompareTag("Wrench") && playerController.isSwinging && !lockedDoor.isMoving ))
         {
             isFlipped = !isFlipped; // Toggle the lever state
+            StartCoroutine(OpenDoor());
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        highlight.unHighlight();
+
+    }
+
+    IEnumerator OpenDoor()
+    {
+        yield return new WaitForSeconds(.29f);
             if (lockedDoor != null) // Runs the function if the door is not null
             {
                 if (isFlipped)
@@ -81,6 +117,35 @@ public class Lever : MonoBehaviour
                 }
             }
             Debug.Log("Lever flipped, door state toggled");
+        yield return null;
         }
+    IEnumerator OpenVerticleDoor()
+    {
+        yield return new WaitForSeconds(.29f);
+        if (lockedDoor != null) // Runs the function if the door is not null
+        {
+            if (isFlipped)
+            {
+                lockedDoor.OpenDoor(); // Call the OpenDoor method in the LockedDoor script
+            }
+            else
+            {
+                lockedDoor.CloseDoor(); // Call the CloseDoor method in the LockedDoor script
+            }
+        }
+        else if (verticalLockedDoor != null) // Runs the function if the vertical door is not null
+        {
+            if (isFlipped)
+            {
+                verticalLockedDoor.OpenDoor(); // Call the OpenDoor method in the VerticalLockedDoor script
+            }
+            else
+            {
+                verticalLockedDoor.CloseDoor(); // Call the CloseDoor method in the VerticalLockedDoor script
+            }
+        }
+        Debug.Log("Lever flipped, door state toggled");
+        yield return null;
     }
 }
+
