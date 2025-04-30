93c01f994cad19d776348f038cc5bd112085c34c
AlexLietha <alexlietha@gmail.com>
2025-04-30 13:47:50 -0500

    Fixed animation

--- Assets/Scripts/VoidKill.cs
+++ Assets/Scripts/VoidKill.cs
@@ -39,7 +39,6 @@ public class VoidKill : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Holy Sigma!");
         if (collision.CompareTag("Void"))
         {
             playerSpring.connectedBody = collision.GetComponent<Rigidbody2D>();
